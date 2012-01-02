c8e82c919431689415eaa7f1e7bdcaaf405a81a6
Sherman Chan <sherman.chan@pobox.com>
2012-01-01 22:41:33 -0500

    renamed project to "CloneFinderCore"

--- CloneFinder/DirectoryWalkEventArgs.cs
+++ CloneFinder/DirectoryWalkEventArgs.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CloneFinder
+namespace CloneFinderCore
 {
     public enum DirectoryWalkObjectType
     {
--- CloneFinder/IFileProcessor.cs
+++ CloneFinder/IFileProcessor.cs
@@ -3,7 +3,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 
-namespace CloneFinder
+namespace CloneFinderCore
 {
     public interface IFileProcessor
     {

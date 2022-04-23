6c76c4a84d5a47b3fac9ebfd69b5986770deb8cb
Sherman Chan <sherman@blacktelescopeworkshop.com>
2022-04-22 23:43:41 -0400

    upgrade assistant run

--- CloneFinderFrontEnd/Program.cs
+++ CloneFinderFrontEnd/Program.cs
@@ -14,6 +14,7 @@ namespace CloneFinderFrontEnd
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
         }

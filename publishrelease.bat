e945d5626022a868863ac541f7bae8b19574380d
S T Chan <stchan@users.noreply.github.com>
2025-02-17 07:32:41 -0500

    fix publishrelease.bat paths

--- publishrelease.bat
+++ publishrelease.bat
@@ -5,9 +5,9 @@ if EXIST "%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\Tools\Vs
     msbuild CloneFinder.sln  -t:Rebuild -p:Configuration=Release -p:Platform=x64 -r
     if NOT EXIST "publish\signed" mkdir "publish\signed"
     del /q "publish\signed\*"
-    copy /y  "publish\x64\CloneFinder_x64_*.msi" "publish\signed\"
+    copy /y  "publish\x64\CloneFinder_*.msi" "publish\signed\"
     cd "publish\signed\"
-    signtool sign /sha1 %CodeSignHash% /t http://time.certum.pl /fd sha256 /v CloneFinder_x64_*.msi
+    signtool sign /sha1 %CodeSignHash% /t http://time.certum.pl /fd sha256 /v CloneFinder_*.msi
 ) ELSE (
     echo Could not set build tools environment.
     exit 1
